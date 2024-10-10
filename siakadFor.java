8a6d39120fcaf61b8700039ca4eb08380fb56a78
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-10-10 09:44:40 +0700

    pertanyaan

--- siakadFor.java
+++ siakadFor.java
@@ -5,7 +5,7 @@ public class siakadFor{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi=0, terendah=100;
+        double nilai, tertinggi=0, terendah=100, mahasiswaLulus=0, mahasiswaTidakLulus=0;
 
         for (int i = 1; i <= 10; i++){   
             System.out.print("Masukkan nilai mahasiswa ke-"+ i+" : ");
@@ -15,9 +15,15 @@ public class siakadFor{
             }
             if (nilai<terendah) {
                 terendah=nilai;
-            }    
+            }if (nilai>=60){
+                mahasiswaLulus+=1;
+            }else{
+                mahasiswaTidakLulus+=1;
+            }
         }
         System.out.println("Nilai tertinggi : "+tertinggi);
         System.out.println("Nilai terendah : "+terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: "+mahasiswaLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: "+mahasiswaTidakLulus);
     }
 }
